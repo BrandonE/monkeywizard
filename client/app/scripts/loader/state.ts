c3a74d4d58eb3f80e66b58c4071f6a796766a8a5
AJP <allenatmc@gmail.com>
2016-01-30 19:39:34 -0600

    add menu backdrop, text, and spinning bananas

--- client/app/scripts/loader/state.ts
+++ client/app/scripts/loader/state.ts
@@ -5,6 +5,7 @@ namespace Loader {
   export class State extends Phaser.State {
     preload() {
       this.load.atlasJSONHash('sprites', 'assets/images/sprites.png', 'assets/images/sprites.json');
+      this.load.image('backdrop', 'assets/images/backdrop.jpg');
     }
 
     create() {
