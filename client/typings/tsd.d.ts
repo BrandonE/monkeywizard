061395c6f1737c8801ae17798384a4da740006f6
Sean Spradlin <seanspradlin@gmail.com>
2016-01-30 19:33:52 -0600

    add wasd to tsd.d.ts

--- client/typings/tsd.d.ts
+++ client/typings/tsd.d.ts
@@ -1,9 +1,9 @@
 /// <reference path="phaser.d.ts" />
 /// <reference path="../app/scripts/generic/cursor.ts" />
+/// <reference path="../app/scripts/generic/wasd.ts" />
 /// <reference path="../app/scripts/boot/state.ts" />
 /// <reference path="../app/scripts/loader/state.ts" />
 /// <reference path="../app/scripts/menu/state.ts" />
 /// <reference path="../app/scripts/attack/state.ts" />
 /// <reference path="../app/scripts/defend/state.ts" />
 /// <reference path="../app/scripts/score/state.ts" />
-
