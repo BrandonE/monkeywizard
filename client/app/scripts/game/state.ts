636808ef4be3306d14a3a0efb9969b1aa81749b1
Brandon Evans <bevans@smartvue.com>
2016-01-31 03:30:28 -0600

    Have the minions spawn during the defense phase.

--- client/app/scripts/game/state.ts
+++ client/app/scripts/game/state.ts
@@ -33,9 +33,7 @@ namespace Game {
         }
 
         attack() {
-            var x: number,
-                y: number,
-                angle: { degrees: number, degreesCounterClockwise: number, radians: number },
+            var angle: { degrees: number, degreesCounterClockwise: number, radians: number },
                 destinationX: number,
                 destinationY: number,
                 graphics: Phaser.Graphics;
@@ -81,53 +79,25 @@ namespace Game {
                 );
 
                 this.add.text(this.player.x - 10, this.player.y, this.banana.toString(), {});
-
-                if (angle.degreesCounterClockwise >= 45 && angle.degreesCounterClockwise < 135) {
-                    x = this.player.x +
-                        (((this.player.x * 2) * Math.cos(angle.radians)) / (2 * Math.sin(angle.radians)));
-                    y = this.game.height - 40;
-                }
-
-                if (angle.degreesCounterClockwise >= 135 && angle.degreesCounterClockwise < 225) {
-                    x = this.game.width - 40;
-                    y = this.player.y +
-                        (((this.player.y * 2) * Math.sin(angle.radians)) / (2 * Math.cos(angle.radians)));
-                }
-
-                if (angle.degreesCounterClockwise >= 225 && angle.degreesCounterClockwise < 315) {
-                    x = (this.game.width - this.player.x) +
-                        (((this.player.x * 2) * Math.cos(angle.radians)) / (2 * Math.sin(angle.radians)));
-                    y = 0;
-                    x = this.game.width - x;
-                }
-
-                if (angle.degreesCounterClockwise >= 315 || angle.degreesCounterClockwise < 45) {
-                    x = 0;
-                    y = (this.game.height - this.player.y) +
-                        (((this.player.y * 2) * Math.sin(angle.radians)) / (2 * Math.cos(angle.radians)));
-                    y = this.game.height - y;
-                }
-
-                if (x > this.game.width - 40) {
-                    x = this.game.width - 40;
-                }
-
-                if (y > this.game.height - 40) {
-                    y = this.game.height - 40;
-                }
-
-                // Minion
-                this.add.text(
-                    x,
-                    y,
-                    'M',
-                    {}
-                );
             }
         }
 
-        defend(waves: Object[][]) {
+        defend(waves: { player_x: number, player_y: number, pointer_x: number, pointer_y: number }[][]) {
             console.log('Defend');
+
+            var wave: { player_x: number, player_y: number, pointer_x: number, pointer_y: number }[],
+                banana: { player_x: number, player_y: number, pointer_x: number, pointer_y: number },
+                w: number,
+                b: number;
+
+            for (w = 0; w < waves.length; w++) {
+                wave = waves[w];
+
+                for (b = 0; b < wave.length; b++) {
+                    banana = wave[b];
+                    this.addMinion(banana, b);
+                }
+            }
         }
 
         getAngle(x1: number, y1: number, x2: number, y2: number) {
@@ -150,8 +120,55 @@ namespace Game {
             };
         }
 
-        createMinion() {
+        addMinion(
+            banana: { player_x: number, player_y: number, pointer_x: number, pointer_y: number }, index: number
+        ) {
+            var angle: { degrees: number, degreesCounterClockwise: number, radians: number },
+                x: number,
+                y: number;
 
+            angle = this.getAngle(banana.player_x, banana.player_y, banana.pointer_x, banana.pointer_y);
+
+            if (angle.degreesCounterClockwise >= 45 && angle.degreesCounterClockwise < 135) {
+                x = this.player.x +
+                    (((this.player.x * 2) * Math.cos(angle.radians)) / (2 * Math.sin(angle.radians)));
+                y = this.game.height - 40;
+            }
+
+            if (angle.degreesCounterClockwise >= 135 && angle.degreesCounterClockwise < 225) {
+                x = this.game.width - 40;
+                y = this.player.y +
+                    (((this.player.y * 2) * Math.sin(angle.radians)) / (2 * Math.cos(angle.radians)));
+            }
+
+            if (angle.degreesCounterClockwise >= 225 && angle.degreesCounterClockwise < 315) {
+                x = (this.game.width - this.player.x) +
+                    (((this.player.x * 2) * Math.cos(angle.radians)) / (2 * Math.sin(angle.radians)));
+                y = 0;
+                x = this.game.width - x;
+            }
+
+            if (angle.degreesCounterClockwise >= 315 || angle.degreesCounterClockwise < 45) {
+                x = 0;
+                y = (this.game.height - this.player.y) +
+                    (((this.player.y * 2) * Math.sin(angle.radians)) / (2 * Math.cos(angle.radians)));
+                y = this.game.height - y;
+            }
+
+            if (x > this.game.width - 40) {
+                x = this.game.width - 40;
+            }
+
+            if (y > this.game.height - 40) {
+                y = this.game.height - 40;
+            }
+
+            this.add.text(
+                x,
+                y,
+                'M' + (index + 1).toString(),
+                {}
+            );
         }
     }
 
