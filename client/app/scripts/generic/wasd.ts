4e43615a5ca8735d2da6d0fe4da9d5f211aad5b8
Sean Spradlin <seanspradlin@gmail.com>
2016-01-30 19:33:39 -0600

    create wasd class

---
+++ client/app/scripts/generic/wasd.ts
@@ -0,0 +1,18 @@
+/// <reference path="../../../typings/tsd.d.ts" />
+'use strict';
+
+namespace Generic {
+  export class WASD {
+      up: Phaser.Key;
+      down: Phaser.Key;
+      left: Phaser.Key;
+      right: Phaser.Key;
+      
+      constructor(game: Phaser.Game) {
+          this.up = game.input.keyboard.addKey(Phaser.Keyboard.W);
+          this.down = game.input.keyboard.addKey(Phaser.Keyboard.S);
+          this.left = game.input.keyboard.addKey(Phaser.Keyboard.A);
+          this.right = game.input.keyboard.addKey(Phaser.Keyboard.D);
+      }
+  }
+}
