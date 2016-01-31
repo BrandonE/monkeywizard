49f94e905a215935d15382a3f7ba025fbd5a97ba
Brandon Evans <bevans@smartvue.com>
2016-01-31 03:18:44 -0600

    More refactoring.

--- client/app/scripts/game/state.ts
+++ client/app/scripts/game/state.ts
@@ -5,7 +5,7 @@ namespace Game {
     export class State extends Phaser.State {
         player: Player;
         pointer: Phaser.Pointer;
-        waves: Object[][] = [[]];
+        waves: { player_x: number, player_y: number, pointer_x: number, pointer_y: number }[][] = [[]];
         attacking: boolean = true;
         banana: number = 0;
         maxBanana: number = 30;
@@ -33,33 +33,21 @@ namespace Game {
         }
 
         attack() {
-            var playerPoint: Phaser.Point,
-                mousePoint: Phaser.Point,
-                x: number,
+            var x: number,
                 y: number,
-                angleDegrees: number,
-                angleRadians: number,
-                angleDegreesCounterClockwise: number,
+                angle: { degrees: number, degreesCounterClockwise: number, radians: number },
                 destinationX: number,
                 destinationY: number,
                 graphics: Phaser.Graphics;
 
             if (this.banana < this.maxBanana) {
-                playerPoint = new Phaser.Point(this.player.x, this.player.y);
-                mousePoint = new Phaser.Point(this.pointer.x, this.pointer.y);
-                angleDegrees = playerPoint.angle(mousePoint, true);
-                angleRadians = playerPoint.angle(mousePoint);
-
-                if (angleDegrees >= 0) {
-                    angleDegreesCounterClockwise = 360 - angleDegrees;
-                } else {
-                    angleDegreesCounterClockwise = -angleDegrees;
-                }
+                angle = this.getAngle(this.player.x, this.player.y, this.pointer.x, this.pointer.y);
 
                 this.waves[0].push({
-                    angle: angleDegrees,
-                    x: this.player.x,
-                    y: this.player.y
+                    player_x: this.player.x,
+                    player_y: this.player.y,
+                    pointer_x: this.pointer.x,
+                    pointer_y: this.pointer.y
                 });
 
                 this.banana++;
@@ -68,55 +56,55 @@ namespace Game {
                 graphics.beginFill(0xFF3300);
                 graphics.lineStyle(5, 0xffd900, 1);
 
-                destinationX = this.player.x + (50 * Math.cos(angleRadians)) - 100;
-                destinationY = this.player.y + (50 * Math.sin(angleRadians)) - 100;
+                destinationX = this.player.x + (50 * Math.cos(angle.radians)) - 100;
+                destinationY = this.player.y + (50 * Math.sin(angle.radians)) - 100;
 
                 graphics.moveTo(this.player.x - 100, this.player.y - 100);
                 graphics.lineTo(destinationX, destinationY);
 
                 graphics.moveTo(destinationX, destinationY);
                 graphics.lineTo(
-                    this.player.x + (10 * Math.cos(angleRadians + 45)) - 100,
-                    this.player.y + (10 * Math.sin(angleRadians + 45)) - 100
+                    this.player.x + (10 * Math.cos(angle.radians + 45)) - 100,
+                    this.player.y + (10 * Math.sin(angle.radians + 45)) - 100
                 );
 
                 graphics.moveTo(destinationX, destinationY);
                 graphics.lineTo(
-                    this.player.x + (10 * Math.cos(angleRadians - 45)) - 100,
-                    this.player.y + (10 * Math.sin(angleRadians - 45)) - 100
+                    this.player.x + (10 * Math.cos(angle.radians - 45)) - 100,
+                    this.player.y + (10 * Math.sin(angle.radians - 45)) - 100
                 );
 
                 graphics.moveTo(this.player.x - 100, this.player.y - 100);
                 graphics.lineTo(
-                    this.player.x + (50 * Math.cos(angleRadians + Math.PI)) - 100,
-                    this.player.y + (50 * Math.sin(angleRadians + Math.PI)) - 100
+                    this.player.x + (50 * Math.cos(angle.radians + Math.PI)) - 100,
+                    this.player.y + (50 * Math.sin(angle.radians + Math.PI)) - 100
                 );
 
                 this.add.text(this.player.x - 10, this.player.y, this.banana.toString(), {});
 
-                if (angleDegreesCounterClockwise >= 45 && angleDegreesCounterClockwise < 135) {
+                if (angle.degreesCounterClockwise >= 45 && angle.degreesCounterClockwise < 135) {
                     x = this.player.x +
-                        (((this.player.x * 2) * Math.cos(angleRadians)) / (2 * Math.sin(angleRadians)));
+                        (((this.player.x * 2) * Math.cos(angle.radians)) / (2 * Math.sin(angle.radians)));
                     y = this.game.height - 40;
                 }
 
-                if (angleDegreesCounterClockwise >= 135 && angleDegreesCounterClockwise < 225) {
+                if (angle.degreesCounterClockwise >= 135 && angle.degreesCounterClockwise < 225) {
                     x = this.game.width - 40;
                     y = this.player.y +
-                        (((this.player.y * 2) * Math.sin(angleRadians)) / (2 * Math.cos(angleRadians)));
+                        (((this.player.y * 2) * Math.sin(angle.radians)) / (2 * Math.cos(angle.radians)));
                 }
 
-                if (angleDegreesCounterClockwise >= 225 && angleDegreesCounterClockwise < 315) {
+                if (angle.degreesCounterClockwise >= 225 && angle.degreesCounterClockwise < 315) {
                     x = (this.game.width - this.player.x) +
-                        (((this.player.x * 2) * Math.cos(angleRadians)) / (2 * Math.sin(angleRadians)));
+                        (((this.player.x * 2) * Math.cos(angle.radians)) / (2 * Math.sin(angle.radians)));
                     y = 0;
                     x = this.game.width - x;
                 }
 
-                if (angleDegreesCounterClockwise >= 315 || angleDegreesCounterClockwise < 45) {
+                if (angle.degreesCounterClockwise >= 315 || angle.degreesCounterClockwise < 45) {
                     x = 0;
                     y = (this.game.height - this.player.y) +
-                        (((this.player.y * 2) * Math.sin(angleRadians)) / (2 * Math.cos(angleRadians)));
+                        (((this.player.y * 2) * Math.sin(angle.radians)) / (2 * Math.cos(angle.radians)));
                     y = this.game.height - y;
                 }
 
@@ -140,6 +128,30 @@ namespace Game {
 
         defend(waves: Object[][]) {
             console.log('Defend');
+        }
+
+        getAngle(x1: number, y1: number, x2: number, y2: number) {
+            var playerPoint: Phaser.Point = new Phaser.Point(x1, y1),
+                mousePoint: Phaser.Point = new Phaser.Point(x2, y2),
+                angleDegrees: number = playerPoint.angle(mousePoint, true),
+                angleRadians: number = playerPoint.angle(mousePoint),
+                angleDegreesCounterClockwise: number;
+
+            if (angleDegrees >= 0) {
+                angleDegreesCounterClockwise = 360 - angleDegrees;
+            } else {
+                angleDegreesCounterClockwise = -angleDegrees;
+            }
+
+            return {
+                degrees: angleDegrees,
+                degreesCounterClockwise: angleDegreesCounterClockwise,
+                radians: angleRadians
+            };
+        }
+
+        createMinion() {
+
         }
     }
 
