7b9f14b3f9dc3e5229dbaf84895f01dc9df09c5c
Sean Spradlin <seanspradlin@gmail.com>
2016-01-30 20:10:25 -0600

    created generateBananas method

--- client/app/scripts/defend/state.ts
+++ client/app/scripts/defend/state.ts
@@ -4,11 +4,34 @@
 namespace Defend { 
   export class State extends Phaser.State {
     player: Player;
+    bananas: number;
     
-    create() {
+    create(): void {
       console.log('Game Started');
       this.player = this.add.existing(new Player(this));
+      this.bananas = 30;
     }
+    
+    generateBananas(): IVector[] {
+      var vectors = [],
+          xMaxima = this.game.width,
+          yMaxima = this.game.height,
+          angleMaxima = Math.PI * 2;
+      for (var i = 0; i < this.bananas; i++) {
+        var vector: IVector = {
+          x: Math.random() * xMaxima,
+          y: Math.random() * yMaxima,
+          angle: Math.random() * angleMaxima
+        };
+      }
+      return vectors;
+    }
+  }
+  
+  export interface IVector {
+    x: number;
+    y: number;
+    angle: number;
   }
   
   export class Player extends Generic.Cursor {
