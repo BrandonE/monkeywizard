abe0ee3b51196dbc4644a25418d94b2f2cd154ef
Brandon Evans <bevans@smartvue.com>
2016-01-31 22:28:21 -0600

    Play again button.

--- client/scripts/end/state.ts
+++ client/scripts/end/state.ts
@@ -4,34 +4,44 @@
 namespace End {
     export class State extends Phaser.State {
         message: string;
+        background: Phaser.Sprite;
         fanfare: Phaser.Sound;
+        results: Phaser.Text;
+        playAgain: Phaser.Text;
+        menu: Phaser.Text;
         turns;
 
         create() {
-            var self = this,
-                background = this.add.image(0, 0, 'sprites', 'Aztec Temple/Aztec-Temple');
+            var self = this;
+
+            this.background = this.add.image(0, 0, 'sprites', 'Aztec Temple/Aztec-Temple');
 
             this.fanfare = this.sound.play('fanfare', 100);
 
-            background.height = this.game.height;
-            background.width = this.game.width;
+            this.background.height = this.game.height;
+            this.background.width = this.game.width;
 
-            var text = this.add.text(this.world.centerX, this.world.centerY - 100, this.message, { font: "60px Arial", fill: "#ffff00", align: "center" });
+            this.results = this.add.text(this.world.centerX, this.world.centerY - 100, this.message, { font: "60px Arial", fill: "#ffff00", align: "center" });
+            this.results.anchor.set(0.5);
 
-            var backButton = this.add.text(this.world.centerX, this.world.centerY + 100, "Back to Menu", { font: "65px Arial", fill: "#ffff00", align: "center" });
+            this.playAgain = this.add.text(this.world.centerX, this.world.centerY + 100, "Play Again", { font: "65px Arial", fill: "#ffff00", align: "center" });
+            this.playAgain.anchor.set(0.5);
+            this.playAgain.inputEnabled = true;
 
-            // text.tint = 0xff00033;
-
-            text.anchor.set(0.5);
-            backButton.anchor.set(0.5);
-
-            backButton.inputEnabled = true;
-            backButton.events.onInputDown.add(goBack, this);
-
-            function goBack() {
+            this.playAgain.events.onInputDown.add(function() {
                 self.fanfare.pause();
-                self.game.state.start('Menu');;
-            }
+                self.game.state.start('Game');
+            }, this);
+
+            this.menu = this.add.text(this.world.centerX, this.world.centerY + 180, "Back to Menu", { font: "65px Arial", fill: "#ffff00", align: "center" });
+            this.menu.anchor.set(0.5);
+            this.menu.inputEnabled = true;
+
+            this.menu.events.onInputDown.add(function() {
+                self.fanfare.pause();
+                self.game.state.start('Menu');
+            }, this);
+
         }
     }
 }
