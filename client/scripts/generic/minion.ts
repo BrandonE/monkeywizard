f842cb360656d9bc647d4d02c78799b236c9d25a
Brandon Evans <bevans@smartvue.com>
2016-02-07 13:02:51 -0600

    Added animation for minions.

--- client/scripts/generic/minion.ts
+++ client/scripts/generic/minion.ts
@@ -3,10 +3,17 @@
 
 namespace Generic {
     export class Minion extends Phaser.Sprite {
+        waveIndex: number;
+        bananaIndex: number;
+        x: number;
+        y: number;
+        player_x: number;
+        player_y: number;
         killed: boolean = false;
 
         constructor(
-            state: Phaser.State, waveIndex, bananaIndex: number, x: number, y: number, player_x: number, player_y
+            state: Phaser.State, waveIndex: number, bananaIndex: number, x: number, y: number, player_x: number,
+            player_y: number
         ) {
             super(
                 state.game,
@@ -19,23 +26,56 @@ namespace Generic {
 
             var self = this;
 
+            this.waveIndex = waveIndex;
+            this.bananaIndex = bananaIndex;
+            this.x = x;
+            this.y = y;
+            this.player_x = player_x;
+            this.player_y = player_y;
+
             this.anchor.setTo(0.5);
             this.game.physics.arcade.enable(this);
             this.body.allowRotation = false;
 
             setTimeout(function() {
-                if (!self.killed && self.game.state.states.Game.id) {
-                    self.game.state.states.Game.bananas.push(
-                        self.game.state.states.Game.game.add.existing(
-                            new Generic.Banana(state, waveIndex, bananaIndex, x, y, player_x, player_y)
-                        )
-                    );
-                }
+                self.secondFrame();
             }, bananaIndex * 100);
 
             setTimeout(function() {
                 self.kill();
             }, 5000);
+        }
+
+        secondFrame() {
+            var self = this;
+
+            this.frameName = 'Monkey_Minion/' + ((this.y > this.game.height / 2) ? 'Back' : 'Front') +
+                '/Monkey_Minion_2Y';
+
+            setTimeout(function() {
+                self.thirdFrame();
+            }, 100);
+        }
+
+        thirdFrame() {
+            this.frameName = 'Monkey_Minion/' + ((this.y > this.game.height / 2) ? 'Back' : 'Front') +
+                '/Monkey_Minion_3';
+
+            if (!this.killed && this.game.state.states.Game.id) {
+                this.game.state.states.Game.bananas.push(
+                    this.game.state.states.Game.game.add.existing(
+                        new Generic.Banana(
+                            this.game.state.states.Game,
+                            this.waveIndex,
+                            this.bananaIndex,
+                            this.x,
+                            this.y,
+                            this.player_x,
+                            this.player_y
+                        )
+                    )
+                );
+            }
         }
     }
 }
